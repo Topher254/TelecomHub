fe1c8ca8db319feb63bdafc04b015fc994fc7404
Topher254 <raphaelsarota@gmail.com>
2025-01-09 10:35:31 +0300

    sign up and login form

---
+++ Frontend/script.js
@@ -0,0 +1,13 @@
+function showSignIn() {
+    document.getElementById('signInForm').style.display = 'flex';
+  }
+  
+  function showRegister() {
+    document.getElementById('registerForm').style.display = 'flex';
+  }
+  
+  function closeForm() {
+    document.getElementById('signInForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'none';
+  }
+  
